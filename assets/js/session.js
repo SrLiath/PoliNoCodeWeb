b3bb42779098c290387ffccdd595ade4d3113e56
LuanC-1 <luan.costa2590@gmail.com>
2024-04-05 17:38:59 -0300

    Criação de Hash para sessão

--- assets/js/session.js
+++ assets/js/session.js
@@ -1,9 +1,19 @@
 const session = require('express-session');
+const crypto = require('crypto');
 
 module.exports = function configureSession(app) {
+    // Gerar uma chave secreta aleatória
+    const secretKey = generateSecretKey();
+
+    // Configurar a sessão com a chave secreta
     app.use(session({
-        secret: 'polibotweb-secret',
+        secret: secretKey,
         resave: false,
         saveUninitialized: false
     }));
+
+    // Função para gerar uma chave secreta hexadecimal aleatória
+    function generateSecretKey() {
+        return crypto.randomBytes(32).toString('hex');
+    }
 };
