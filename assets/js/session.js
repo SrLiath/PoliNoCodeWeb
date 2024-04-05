14f455fb7ace8970014934866fbc7514d0f6ce5b
LuanC-1 <luan.costa2590@gmail.com>
2024-04-05 17:21:57 -0300

    Atualização chave de sessão

--- assets/js/session.js
+++ assets/js/session.js
@@ -2,7 +2,7 @@ const session = require('express-session');
 
 module.exports = function configureSession(app) {
     app.use(session({
-        secret: 'sua_chave_secreta',
+        secret: 'polibotweb-secret',
         resave: false,
         saveUninitialized: false
     }));
