6b63cfee82be7935dd3d563e3206b18eb7f143ce
LuanC-1 <luan.costa2590@gmail.com>
2024-04-09 13:45:40 -0300

    Atualização dos arquivos SQL

--- Mysql/Script.sql
+++ Mysql/Script.sql
@@ -1,6 +1,8 @@
 CREATE DATABASE db_polibotweb;
 USE db_polibotweb;
 
+#Criação tabela de usuário
+
 CREATE TABLE tb_usuario (
     id              INT NOT NULL AUTO_INCREMENT,
     nome            VARCHAR(65),
@@ -11,3 +13,29 @@ CREATE TABLE tb_usuario (
 
 # ALTER USER 'polibotweb'@'localhost' IDENTIFIED WITH mysql_native_password BY 'polibotweb'; (Caso tenha erro para conectar pelo Node JS)
 
+#Criação tabela de key
+
+CREATE TABLE tb_key (
+    id              INT NOT NULL AUTO_INCREMENT,
+    usuario_id      INT,
+    chave           VARCHAR(255),
+    compra        DATE,
+    expiracao_key   DATE,
+    PRIMARY KEY(id),
+    FOREIGN KEY (usuario_id) REFERENCES tb_usuario(id)
+);
+
+#Criação de Trigger para criar usuario na tabela key
+
+DELIMITER $$
+
+CREATE TRIGGER criar_usuario_tb_key AFTER INSERT ON tb_usuario
+FOR EACH ROW
+BEGIN
+    INSERT INTO tb_key (usuario_id) VALUES (NEW.id);
+END$$
+
+DELIMITER ;
+
+#Visualizar dados da TRIGGER
+SHOW CREATE TRIGGER criar_usuario_tb_key;
