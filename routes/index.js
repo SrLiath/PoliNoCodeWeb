7dd147a672d1cb060ff02245a5246848447b199b
LuanC-1 <luan.costa2590@gmail.com>
2024-04-15 15:27:58 -0300

    Concluida pagina de visualizar chaves

--- routes/index.js
+++ routes/index.js
@@ -85,12 +85,166 @@ router.get('/seekeys', (req, res) => {
             return res.status(500).send('Erro interno do servidor');
         }
 
-        // Enviar os dados das chaves como resposta em formato JSON
-        res.json(results);
+        // Renderize o template HTML com os dados das chaves
+        const keysHtml = `
+            <!DOCTYPE html>
+            <html>
+              <head>
+                <!-- Basic -->
+                <meta charset="utf-8" />
+                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                <!-- Mobile Metas -->
+                <meta
+                  name="viewport"
+                  content="width=device-width, initial-scale=1, shrink-to-fit=no"
+                />
+                <!-- Site Metas -->
+                <link rel="icon" href="../images/logo_name.png" type="image/gif" />
+                <meta name="keywords" content="" />
+                <meta name="description" content="" />
+                <meta name="author" content="" />
+
+                <title>Polibot - Minhas Chaves</title>
+
+                <!-- bootstrap core css -->
+                <link rel="stylesheet" type="text/css" href="../css/bootstrap.css" />
+
+                <!-- fonts style -->
+                <link
+                  href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap"
+                  rel="stylesheet"
+                />
+
+                <!-- font awesome style -->
+                <link href="../css/font-awesome.min.css" rel="stylesheet" />
+
+                <!-- Custom styles for this template -->
+                <link href="../css/style.css" rel="stylesheet" />
+                <!-- responsive style -->
+                <link href="../css/responsive.css" rel="stylesheet" />
+              </head>
+
+              <body class="sub_page">
+                <div class="hero_area">
+                  <!-- header section strats -->
+                  <header class="header_section">
+                    <div class="container-fluid">
+                      <nav class="navbar navbar-expand-lg custom_nav-container">
+                        <a class="navbar-brand" href="../html/index.html">
+                          <span>
+                            <img src="../images/logo_name.png" alt="Logo" />
+                            Polibot</span
+                          >
+                        </a>
+
+                        <button
+                          class="navbar-toggler"
+                          type="button"
+                          data-toggle="collapse"
+                          data-target="#navbarSupportedContent"
+                          aria-controls="navbarSupportedContent"
+                          aria-expanded="false"
+                          aria-label="Toggle navigation"
+                        >
+                          <span class=""> </span>
+                        </button>
+
+                        <div
+                          class="collapse navbar-collapse"
+                          id="navbarSupportedContent"
+                        >
+                          <ul class="navbar-nav ml-auto">
+                            <li class="nav-item">
+                              <a class="nav-link" href="../html/index.html"
+                                >Home <span class="sr-only">(current)</span></a
+                              >
+                            </li>
+                            <li class="nav-item active">
+                              <a class="nav-link" href="../html/about.html"> Sobre</a>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link" href="../html/price.html">Preços</a>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link" href="../html/suport.html">Suporte</a>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link" href="../html/login.html">Login</a>
+                            </li>
+                          </ul>
+                        </div>
+                      </nav>
+                    </div>
+                  </header>
+                </div>
+                <!-- end header section -->
+
+                <!-- See Keys section -->
+                <section class="selection_panel">
+                  <div class="selection_panel_container">
+                    <ul>
+                      <li><a href="/seeregistration">Ver Cadastro</a></li>
+                      <li><a href="/seekeys">Minhas Chaves</a></li>
+                      <li><a href="/myrequests">Meus Pedidos</a></li>
+                      <li><a href="/sale">Adquirir Plano</a></li>
+                    </ul>
+                  </div>
+
+                  <div class="purchase_container">
+                    <div class="purchase_heading_container">
+                      <h2>Minhas Chaves</h2>
+                      <br />
+                    </div>
+                    <div class="purchase_row">
+                      <div class="purchase_col-md-8 purchase_col-lg-6">
+                        <div class="purchase_form_container">
+                          <table class="table">
+                            <thead>
+                              <tr>
+                                <th>Chave</th>
+                                <th>Compra</th>
+                                <th>Expiração</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              ${results
+                .map(
+                    (key) => `
+                                      <tr>
+                                          <td>${key.chave}</td>
+                                          <td>${key.compra}</td>
+                                          <td>${key.expiracao_key}</td>
+                                      </tr>
+                                  `
+                )
+                .join("")}
+                            </tbody>
+                          </table>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </section>
+                <!-- End See Keys section -->
+
+                <!-- footer section -->
+                <footer class="footer_section">
+                  <div class="container">
+                    <p>
+                      &copy; <span id="displayYear"></span> Todos os direitos reservados por
+                      <a href="../html/index.html">Polibot </a>
+                    </p>
+                    <br />
+                  </div>
+                </footer>
+              </body>
+            </html>
+        `;
+
+        // Envie o HTML como resposta
+        res.send(keysHtml);
     });
 });
-
-
 
 // Rota para lidar com o envio do formulário de registro
 router.post('/register', (req, res) => {
@@ -114,5 +268,6 @@ router.post('/register', (req, res) => {
         return res.status(200).send('Usuário cadastrado com sucesso');
     });
 });
+
 
 module.exports = router;
