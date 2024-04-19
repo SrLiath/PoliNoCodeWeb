7bf72b50786390176a27c669363f63346ef7f6d4
LuanC-1 <luan.costa2590@gmail.com>
2024-04-18 22:32:24 -0300

    Ajuste de todas as telas após login

--- Mysql/Script.sql
+++ Mysql/Script.sql
@@ -1,9 +1,5 @@
-CREATE DATABASE db_polibotweb;
-USE db_polibotweb;
-
-#Criação tabela de usuário
-
-CREATE TABLE tb_usuario (
+-- Criação da tabela para armazenar informações dos usuários
+CREATE TABLE IF NOT EXISTS tb_usuario (
     id              INT NOT NULL AUTO_INCREMENT,
     nome            VARCHAR(65),
     email           VARCHAR(255),    
@@ -11,11 +7,8 @@ CREATE TABLE tb_usuario (
     PRIMARY KEY(id)
 );
 
-# ALTER USER 'polibotweb'@'localhost' IDENTIFIED WITH mysql_native_password BY 'polibotweb'; (Caso tenha erro para conectar pelo Node JS)
-
-#Criação tabela de key
-
-CREATE TABLE tb_key (
+-- Criação da tabela para armazenar as chaves relacionadas aos usuários
+CREATE TABLE IF NOT EXISTS tb_key (
     id              INT NOT NULL AUTO_INCREMENT,
     usuario_id      INT,
     chave           VARCHAR(255),
@@ -25,30 +18,41 @@ CREATE TABLE tb_key (
     FOREIGN KEY (usuario_id) REFERENCES tb_usuario(id)
 );
 
-#Criação de Trigger para criar usuario na tabela key
+-- Criação da tabela para armazenar informações das compras dos usuários
+CREATE TABLE IF NOT EXISTS tb_compra (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario_id INT NOT NULL,
+    nome_cartao VARCHAR(255) NOT NULL,
+    numero_cartao VARCHAR(20) NOT NULL,
+    data_validade VARCHAR(10) NOT NULL,
+    codigo_seguranca VARCHAR(3) NOT NULL,
+    plano ENUM('standard', 'premium', 'business') NOT NULL,
+    compra_aprovada BOOLEAN DEFAULT true,
+    data_transacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (usuario_id) REFERENCES tb_usuario(id)
+);
 
+-- Criação de trigger para criar uma chave quando um usuário é inserido
 DELIMITER $$
-
-CREATE TRIGGER criar_usuario_tb_key AFTER INSERT ON tb_usuario
+CREATE TRIGGER criar_usuario_tb_key_tb_compra AFTER INSERT ON tb_usuario
 FOR EACH ROW
 BEGIN
     INSERT INTO tb_key (usuario_id) VALUES (NEW.id);
 END$$
-
 DELIMITER ;
 
-#Visualizar dados da TRIGGER
-SHOW CREATE TRIGGER criar_usuario_tb_key;
+-- Criação de trigger para gerar uma chave quando uma compra é inserida e aprovada
+DELIMITER $$
+CREATE TRIGGER after_compra_insert
+AFTER INSERT ON tb_compra
+FOR EACH ROW
+BEGIN
+    DECLARE nova_data DATE;
+    IF NEW.compra_aprovada = 1 THEN
+        SET nova_data = DATE_ADD(NEW.data_transacao, INTERVAL 30 DAY);
 
-#Criação tabela de compra :
-
-CREATE TABLE tb_compra (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome_cartao VARCHAR(255) NOT NULL,
-    numero_cartao VARCHAR(16) NOT NULL,
-    data_validade VARCHAR(7) NOT NULL,
-    codigo_seguranca VARCHAR(4) NOT NULL,
-    plano ENUM('standard', 'premium', 'business') NOT NULL,
-    compra_aprovada BOOLEAN DEFAULT true,
-    data_transacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+        INSERT INTO tb_key (usuario_id, chave, compra, expiracao_key)
+        VALUES (NEW.usuario_id, UUID(), NEW.data_transacao, nova_data);
+    END IF;
+END$$
+DELIMITER ;
--- routes/index.js
+++ routes/index.js
@@ -1,87 +1,89 @@
-const express = require('express');
-const path = require('path');
-const pool = require('../assets/db/connection');
+const express = require("express");
+const path = require("path");
+const pool = require("../assets/db/connection");
 
 const router = express.Router();
 
 // Rota para a página inicial
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'assets', 'html', 'index.html'));
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "assets", "html", "index.html"));
 });
 
 // Rota para a página de registro
-router.get('/register', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'assets', 'html', 'register.html'));
+router.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "assets", "html", "register.html"));
 });
 
 // Rota para lidar com o envio do formulário de login
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   const { email, senha } = req.body;
 
   // Verificar se o email e a senha estão presentes
   if (!email || !senha) {
-    return res.status(400).send('Por favor, preencha todos os campos');
+    return res.status(400).send("Por favor, preencha todos os campos");
   }
 
   // Consultar o banco de dados para verificar as credenciais do usuário
-  pool.query('SELECT * FROM tb_usuario WHERE email = ? AND senha = ?', [email, senha], (err, results) => {
-    if (err) {
-      console.error('Erro ao consultar usuário:', err);
-      return res.status(500).send('Erro interno do servidor');
+  pool.query(
+    "SELECT * FROM tb_usuario WHERE email = ? AND senha = ?",
+    [email, senha],
+    (err, results) => {
+      if (err) {
+        console.error("Erro ao consultar usuário:", err);
+        return res.status(500).send("Erro interno do servidor");
+      }
+
+      // Verificar se o usuário foi encontrado
+      if (results.length === 0) {
+        return res.status(401).send("Email ou senha incorretos");
+      }
+
+      // Definir a sessão de autenticação
+      req.session.authenticated = true;
+      req.session.user = results[0]; // Armazenar informações do usuário na sessão
+
+      return res.redirect("/workspace");
     }
-
-    // Verificar se o usuário foi encontrado
-    if (results.length === 0) {
-      return res.status(401).send('Email ou senha incorretos');
-    }
-
-    // Definir a sessão de autenticação
-    req.session.authenticated = true;
-    req.session.user = results[0]; // Armazenar informações do usuário na sessão
-
-    return res.redirect('/workspace');
-  });
+  );
 });
 
 // Rota para a tela de workspace
-router.get('/workspace', (req, res) => {
+router.get("/workspace", (req, res) => {
   // Verificar se o usuário está autenticado
   if (!req.session.authenticated) {
-    return res.status(401).send('Acesso não autorizado');
+    return res.status(401).send("Acesso não autorizado");
   }
 
-  res.sendFile(path.join(__dirname, '..', 'assets', 'html', 'workspace.html'));
-});
-
-// Rota para lidar com pedidos (Myrequest)
-router.get('/myrequests', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'assets', 'html', 'myrequests.html'));
+  res.sendFile(path.join(__dirname, "..", "assets", "html", "workspace.html"));
 });
 
 // Rota para lidar com venda (sale)
-router.get('/sale', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'assets', 'html', 'sale.html'));
+router.get("/sale", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "assets", "html", "sale.html"));
 });
 
 // Rota para lidar com chaves (seekeys)
-router.get('/seekeys', (req, res) => {
+router.get("/seekeys", (req, res) => {
   // Verificar se o usuário está autenticado
   if (!req.session.authenticated) {
-    return res.status(401).send('Acesso não autorizado');
+    return res.status(401).send("Acesso não autorizado");
   }
 
   // Obter o ID do usuário da sessão
   const usuario_id = req.session.user.id;
 
   // Consultar o banco de dados para obter as chaves do usuário
-  pool.query('SELECT chave, compra, expiracao_key FROM tb_key WHERE usuario_id = ?', [usuario_id], (err, results) => {
-    if (err) {
-      console.error('Erro ao consultar chaves do usuário:', err);
-      return res.status(500).send('Erro interno do servidor');
-    }
+  pool.query(
+    "SELECT chave, compra, expiracao_key FROM tb_key WHERE usuario_id = ?",
+    [usuario_id],
+    (err, results) => {
+      if (err) {
+        console.error("Erro ao consultar chaves do usuário:", err);
+        return res.status(500).send("Erro interno do servidor");
+      }
 
-    // Renderize o template HTML com os dados das chaves
-    const keysHtml = `
+      // Renderize o template HTML com os dados das chaves
+      const keysHtml = `
             <!DOCTYPE html>
             <html>
               <head>
@@ -200,16 +202,16 @@ router.get('/seekeys', (req, res) => {
                             </thead>
                             <tbody>
                               ${results
-        .map(
-          (key) => `
+                                .map(
+                                  (key) => `
                                       <tr>
                                           <td>${key.chave}</td>
                                           <td>${key.compra}</td>
                                           <td>${key.expiracao_key}</td>
                                       </tr>
                                   `
-        )
-        .join("")}
+                                )
+                                .join("")}
                             </tbody>
                           </table>
                         </div>
@@ -233,54 +235,63 @@ router.get('/seekeys', (req, res) => {
             </html>
         `;
 
-    // Envie o HTML como resposta
-    res.send(keysHtml);
-  });
+      // Envie o HTML como resposta
+      res.send(keysHtml);
+    }
+  );
 });
 
 // Rota para lidar com o envio do formulário de registro
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   const { nome, email, senha, confirmar_senha } = req.body;
 
   // Verificar se todos os campos estão presentes
   if (!nome || !email || !senha || !confirmar_senha) {
-    return res.status(400).send('Por favor, preencha todos os campos');
+    return res.status(400).send("Por favor, preencha todos os campos");
   }
 
   // Verificar se a senha e a confirmação de senha correspondem
   if (senha !== confirmar_senha) {
-    return res.status(400).send('A senha e a confirmação de senha não correspondem');
+    return res
+      .status(400)
+      .send("A senha e a confirmação de senha não correspondem");
   }
 
-  pool.query('INSERT INTO tb_usuario (nome, email, senha) VALUES (?, ?, ?)', [nome, email, senha], (err, results) => {
-    if (err) {
-      console.error('Erro ao inserir usuário:', err);
-      return res.status(500).send('Erro interno do servidor');
+  pool.query(
+    "INSERT INTO tb_usuario (nome, email, senha) VALUES (?, ?, ?)",
+    [nome, email, senha],
+    (err, results) => {
+      if (err) {
+        console.error("Erro ao inserir usuário:", err);
+        return res.status(500).send("Erro interno do servidor");
+      }
+      return res.status(200).send("Usuário cadastrado com sucesso");
     }
-    return res.status(200).send('Usuário cadastrado com sucesso');
-  });
+  );
 });
 
-
 // Rota para lidar com a visualização e atualização do registro do usuário
-router.get('/seeregistration', (req, res) => {
+router.get("/seeregistration", (req, res) => {
   // Verificar se o usuário está autenticado
   if (!req.session.authenticated) {
-    return res.status(401).send('Acesso não autorizado');
+    return res.status(401).send("Acesso não autorizado");
   }
 
   // Obter o ID do usuário da sessão
   const usuario_id = req.session.user.id;
 
   // Consultar o banco de dados para obter os detalhes do usuário
-  pool.query('SELECT nome, email FROM tb_usuario WHERE id = ?', [usuario_id], (err, results) => {
-    if (err) {
-      console.error('Erro ao consultar detalhes do usuário:', err);
-      return res.status(500).send('Erro interno do servidor');
-    }
+  pool.query(
+    "SELECT nome, email FROM tb_usuario WHERE id = ?",
+    [usuario_id],
+    (err, results) => {
+      if (err) {
+        console.error("Erro ao consultar detalhes do usuário:", err);
+        return res.status(500).send("Erro interno do servidor");
+      }
 
-    // Renderize o template HTML com os detalhes do usuário
-    const registrationHtml = `
+      // Renderize o template HTML com os detalhes do usuário
+      const registrationHtml = `
     <!DOCTYPE html>
     <html>
       <head>
@@ -531,16 +542,17 @@ router.get('/seeregistration', (req, res) => {
     
     `;
 
-    // Envie o HTML como resposta
-    res.send(registrationHtml);
-  });
+      // Envie o HTML como resposta
+      res.send(registrationHtml);
+    }
+  );
 });
 
 // Rota para lidar com a atualização do registro do usuário
-router.post('/updateRegistration', (req, res) => {
+router.post("/updateRegistration", (req, res) => {
   // Verificar se o usuário está autenticado
   if (!req.session.authenticated) {
-    return res.status(401).send('Acesso não autorizado');
+    return res.status(401).send("Acesso não autorizado");
   }
 
   // Obter os dados do formulário
@@ -548,55 +560,240 @@ router.post('/updateRegistration', (req, res) => {
 
   // Verificar se a nova senha corresponde à confirmação da senha
   if (novaSenha !== confirmarSenha) {
-    return res.status(400).send('A nova senha e a confirmação da senha não correspondem');
+    return res
+      .status(400)
+      .send("A nova senha e a confirmação da senha não correspondem");
   }
 
   // Obter o ID do usuário da sessão
   const usuario_id = req.session.user.id;
 
   // Atualizar o registro do usuário no banco de dados
-  pool.query('UPDATE tb_usuario SET nome = ?, senha = ? WHERE id = ?', [nome, novaSenha, usuario_id], (err, results) => {
-    if (err) {
-      console.error('Erro ao atualizar registro do usuário:', err);
-      return res.status(500).send('Erro interno do servidor');
-    }
+  pool.query(
+    "UPDATE tb_usuario SET nome = ?, senha = ? WHERE id = ?",
+    [nome, novaSenha, usuario_id],
+    (err, results) => {
+      if (err) {
+        console.error("Erro ao atualizar registro do usuário:", err);
+        return res.status(500).send("Erro interno do servidor");
+      }
 
-    // Redirecionar o usuário de volta para a página de visualização do registro
-    res.redirect('/seeregistration');
-  });
+      // Redirecionar o usuário de volta para a página de visualização do registro
+      res.redirect("/seeregistration");
+    }
+  );
 });
 
-
 // Rota para lidar com a compra do plano
-router.post('/purchase', (req, res) => {
+router.post("/purchase", (req, res) => {
   // Verificar se o usuário está autenticado
-  if (!req.session.authenticated) {
-    return res.status(401).send('Acesso não autorizado');
+  if (!req.session.authenticated || !req.session.user.id) {
+    return res.status(401).send("Acesso não autorizado");
   }
 
+  // Obter o ID do usuário da sessão
+  const userId = req.session.user.id;
+
   // Obter os dados do formulário
-  const { nomeCartao, numeroCartao, dataValidade, codigoSeguranca, plano } = req.body;
+  const { card_name, card_number, card_expiry, card_cvv, plano } = req.body;
 
   // Verificar se o plano selecionado é válido
-  const planosValidos = ['standard', 'premium', 'business'];
+  const planosValidos = ["standard", "premium", "business"];
   if (!planosValidos.includes(plano)) {
-    return res.status(400).send('Plano selecionado inválido');
+    return res.status(400).send("Plano selecionado inválido");
   }
 
   // Inserir os dados no banco de dados usando pool.query
+  pool.query(
+    "INSERT INTO tb_compra (usuario_id, nome_cartao, numero_cartao, data_validade, codigo_seguranca, plano) VALUES (?, ?, ?, ?, ?, ?)",
+    [userId, card_name, card_number, card_expiry, card_cvv, plano],
+    (err, results) => {
+      if (err) {
+        console.error(
+          "Erro ao inserir detalhes da compra no banco de dados:",
+          err
+        );
+        return res.status(500).send("Erro interno do servidor");
+      }
 
-  pool.query('INSERT INTO tb_compra (nome_cartao, numero_cartao, data_validade, codigo_seguranca, plano) VALUES (?, ?, ?, ?, ?)', [nomeCartao, numeroCartao, dataValidade, codigoSeguranca, plano], (err, results) => {
-    if (err) {
-      console.error('Erro ao inserir detalhes da compra no banco de dados:', err);
-      return res.status(500).send('Erro interno do servidor');
+      // Redirecionar o usuário
+      res.redirect("/myrequests");
     }
-
-    // Redirecionar o usuário
-    res.redirect('/myrequests');
-  });
+  );
 });
 
+// Rota para lidar com /myrequests
+router.get("/myrequests", (req, res) => {
+  // Verificar se o usuário está autenticado
+  if (!req.session.authenticated) {
+    return res.status(401).send("Acesso não autorizado");
+  }
 
+  // Obter o ID do usuário da sessão
+  const usuario_id = req.session.user.id;
 
+  // Consultar o banco de dados para obter os dados necessários
+  pool.query(
+    "SELECT plano, data_transacao FROM tb_compra WHERE usuario_id = ?",
+    [usuario_id],
+    (err, results) => {
+      if (err) {
+        console.error("Erro ao consultar dados do usuário:", err);
+        return res.status(500).send("Erro interno do servidor");
+      }
+
+      // Renderizar o template HTML com os dados obtidos
+      const htmlTemplate = `
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <!-- Basic -->
+          <meta charset="utf-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <!-- Mobile Metas -->
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+          <!-- Site Metas -->
+          <link rel="icon" href="../images/logo_name.png" type="image/gif" />
+          <meta name="keywords" content="" />
+          <meta name="description" content="" />
+          <meta name="author" content="" />
+      
+          <title>Polibot - Minhas Chaves</title>
+      
+          <!-- bootstrap core css -->
+          <link rel="stylesheet" type="text/css" href="../css/bootstrap.css" />
+      
+          <!-- fonts style -->
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap"
+            rel="stylesheet"
+          />
+      
+          <!-- font awesome style -->
+          <link href="../css/font-awesome.min.css" rel="stylesheet" />
+      
+          <!-- Custom styles for this template -->
+          <link href="../css/style.css" rel="stylesheet" />
+          <!-- responsive style -->
+          <link href="../css/responsive.css" rel="stylesheet" />
+        </head>
+      
+        <body class="sub_page">
+          <div class="hero_area">
+            <!-- header section strats -->
+            <header class="header_section">
+              <div class="container-fluid">
+                <nav class="navbar navbar-expand-lg custom_nav-container">
+                  <a class="navbar-brand" href="../html/index.html">
+                    <span>
+                      <img src="../images/logo_name.png" alt="Logo" />
+                      Polibot</span
+                    >
+                  </a>
+      
+                  <button
+                    class="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                  >
+                    <span class=""> </span>
+                  </button>
+      
+                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto">
+                      <li class="nav-item">
+                        <a class="nav-link" href="../html/index.html"
+                          >Home <span class="sr-only">(current)</span></a
+                        >
+                      </li>
+                      <li class="nav-item active">
+                        <a class="nav-link" href="../html/about.html"> Sobre</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="../html/price.html">Preços</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="../html/suport.html">Suporte</a>
+                      </li>
+                    </ul>
+                  </div>
+                </nav>
+              </div>
+            </header>
+          </div>
+          <!-- end header section -->
+      
+          <!-- See Keys section -->
+          <section class="selection_panel">
+            <div class="selection_panel_container">
+              <ul>
+                <li><a href="/seeregistration">Ver Cadastro</a></li>
+                <li><a href="/seekeys">Minhas Chaves</a></li>
+                <li><a href="/myrequests">Meus Pedidos</a></li>
+                <li><a href="/sale">Adquirir Plano</a></li>
+              </ul>
+            </div>
+      
+            <div class="purchase_container">
+              <div class="purchase_heading_container">
+                <h2>Meus Pedidos</h2>
+                <br />
+              </div>
+              <div class="purchase_row">
+                <div class="purchase_col-md-8 purchase_col-lg-6">
+                  <div class="purchase_form_container">
+                    <table class="table">
+                      <thead>
+                        <tr>
+                          <th>Plano</th>
+                          <th>Data da Transação</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        ${results
+                          .map(
+                            (row) => `
+                        <tr>
+                          <td>${row.plano}</td>
+                          <td>${row.data_transacao}</td>
+                        </tr>
+                        `
+                          )
+                          .join("")}
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+          <!-- End See Keys section -->
+      
+          <!-- footer section -->
+          <footer class="footer_section">
+            <div class="container">
+              <p>
+                &copy; <span id="displayYear"></span> Todos os direitos reservados por
+                <a href="../html/index.html">Polibot </a>
+              </p>
+              <br />
+            </div>
+          </footer>
+        </body>
+      </html>      
+      `;
+
+      // Enviar o HTML como resposta
+      res.send(htmlTemplate);
+    }
+  );
+});
 
 module.exports = router;
